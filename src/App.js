8a8c3c949ed0fb2ff39932a8399fdd157e4fc623
imaikoichiro <kaminishi427@gmail.com>
2021-06-05 13:21:15 +0900

    implement why-jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div><h1>Hello, world!</h1></div>
-  );
+//function App() {
+//  return (
+//    <div>Hello, world!</div>
+//  );
+//}
+
+class App extends Component {
+  render() {
+    return <h1>Hello, World!!</h1>;
+  }
 }
+
+//class App extends Component {
+//  render() {
+//    return React.createElement(
+//      "div",
+//      null,
+//      "Hello, world!!!"
+//    );
+//  }
+//}
 
 export default App;
